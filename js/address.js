5157edec8159af43140b3f4405648c99c2ae27b3
SugarTiger <1029109173@qq.com>
2017-05-16 23:59:37 +0800

    收货地址管理

---
+++ js/address.js
@@ -0,0 +1,68 @@
+$(function() {
+    // 添加地址时表单验证
+    // 收件人
+    $('#addruser').focus(function() {
+        $(this).parent().find('span').html('');
+    });
+    $('#addruser').blur(function() {
+        var value = $.trim($(this).val());
+        if (value == "") {
+            $(this).parent().find('span').html('输入不能为空！');
+        }
+    });
+    // 所在地区
+    $('#area').focus(function() {
+        $(this).parent().find('span').html('');
+    });
+    $('#area').blur(function() {
+        var value = $.trim($(this).val());
+        if (value == "") {
+            $(this).parent().find('span').html('输入不能为空！');
+        }
+    });
+    // 详情地址
+    $('#addr_details').focus(function() {
+        $(this).parent().find('span').html('');
+    });
+    $('#addr_details').blur(function() {
+        var value = $.trim($(this).val());
+        if (value == "") {
+            $(this).parent().find('span').html('输入不能为空！');
+        }
+    });
+    // 手机号码
+    $('#phone').focus(function() {
+        $(this).parent().find('span').html('');
+    });
+    $('#phone').blur(function() {
+        var value = $.trim($(this).val());
+        if (value == "") {
+            $(this).parent().find('span').html('输入不能为空！');
+        } else if (!(/^1[34578]\d{9}$/.test(value))) {
+            $(this).parent().find('span').html('手机号码输入有误！');
+        }
+    });
+    // 固定号码
+    $('#tel').focus(function() {
+        $(this).parent().find('span').html('');
+    });
+    $('#tel').blur(function() {
+        var value = $.trim($(this).val());
+        if (value == "") {
+            $(this).parent().find('span').html('输入不能为空！');
+        }
+    });
+    // 邮箱
+    $('#email').focus(function() {
+        $(this).parent().find('span').html('');
+    });
+    $('#email').blur(function() {
+        var value = $.trim($(this).val());
+        if (value == "") {
+            $(this).parent().find('span').html('输入不能为空！');
+        } else if (!(/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(value))) {
+            $(this).parent().find('span').html('邮箱地址输入有误！');
+        }
+    });
+
+});
--- js/public.js
+++ js/public.js
@@ -1,10 +1,33 @@
 $(function() {
     // 导航下拉
     // toggle() 方法在 jQuery 版本 1.8 中被废弃，在版本 1.9 中被移除。
-    $('.menu>h2').click(function() {
-        $('.submenu').slideToggle("slow", "swing");
-    });
-    $('.submenu').click(function() {
-        $('.menu>h2').trigger("click");
-    });
+    (function() {
+        $('.menu>h2').click(function() {
+            $('.submenu').slideToggle();
+            var atclass = $('.menu>h2').attr('class');
+            $('.menu>h2>img').attr('src', atclass ? 'images/arrows_icon2.png' : 'images/arrows_icon.png');
+            $('.menu>h2').attr('class', atclass ? '' : 'active')
+        });
+        $('.submenu').click(function() {
+            $('.menu>h2').trigger("click");
+        });
+    })();
+    // 搜索框默认提示
+    (function() {
+        $('.search>input').focus(function() {
+            if ($(this).val() == "电饭煲") {
+                $(this).attr('value', '');
+            }
+        });
+        $('.search>input').blur(function() {
+            if ($(this).val() == "") {
+                $(this).attr('value', '电饭煲');
+            }
+        });
+    })();
+    // 侧边栏的高度
+    (function() {
+        var mainHeight = parseFloat($('.main').css('height'));
+        $('.sidebar').css('height', mainHeight + 70 + 'px');
+    })();
 });
